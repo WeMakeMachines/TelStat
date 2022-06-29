e41aab532b9772c1b5fc688f6dfdf972deafff11
Franco Speziali <franco@wemakemachines.com>
2022-06-29 22:29:54 +0200

    Added checkPublisherExistsOnTopic to TopicsDAO

--- source/services/DAO/Topics.ts
+++ source/services/DAO/Topics.ts
@@ -2,7 +2,23 @@ import Topics from "../../models/Topic";
 
 export default class TopicsDAO {
   public static async getById(id: string) {
-    return Topics.findById(id).lean().populate("publishers", ["label"]);
+    return Topics.findById(id).lean().populate("publishers", ["name"]);
+  }
+
+  public static async checkPublisherExistsOnTopic(
+    topicName: string,
+    publisherId: string
+  ) {
+    const topic = await Topics.findOne({
+      name: topicName,
+      publishers: publisherId,
+    });
+
+    if (topic) {
+      return Promise.resolve();
+    }
+
+    return Promise.reject("Topic name and publisher ID mismatch");
   }
 
   public static async getAll() {
